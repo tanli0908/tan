5f2fd9687a2e3fa206c5437b4e2564ab791981c6
谭理 <tanli_0908@163.com>
2022-01-04 22:00:34 +0800

    修改原因：提交测试代码

---
+++ src/main/java/com/tan/test/ForStringInput.java
@@ -0,0 +1,18 @@
+package com.tan.test;
+
+
+/**
+ * @ClassName ForStringInput
+ * @Description TODO
+ * @Author tanli
+ * @Date 2022/1/4
+ * @Since 1.0
+ */
+public class ForStringInput {
+
+
+    public static void main(String[] args) {
+           int aint= Integer.parseInt("220000003");
+           System.out.println(aint);
+    }
+}
